d4da53135d3f709871b4ae04db48eb7e9d3fa5ab
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2013-09-12 22:47:05 -0300

    logs com data e hora

--- backup.cmd
+++ backup.cmd
@@ -1,3 +1,11 @@
 @echo off
 
-toucan --script="dofile(getscript([[Todos os backups]]))" --logfile="backup.log"
+set data_hora=%date%_%time%
+set data_hora=%data_hora: =_%
+set data_hora=%data_hora::=_%
+set data_hora=%data_hora:/=_%
+set data_hora=%data_hora:.=_%
+
+set log=backup_%data_hora%.log
+
+toucan --script="dofile(getscript([[Todos os backups]]))" --logfile="%log%"
---
+++ clean.cmd
@@ -0,0 +1,1 @@
+@del backup*.log
